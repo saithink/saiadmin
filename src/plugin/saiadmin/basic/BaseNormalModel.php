1e9bf40ed7296e9bfeadc46f93c043b206ecfd55
sai <1430792918@qq.com>
2024-05-05 09:46:38 +0800

    增加一个不使用软删除的基本模型

---
+++ src/plugin/saiadmin/basic/BaseNormalModel.php
@@ -0,0 +1,52 @@
+<?php
+// +----------------------------------------------------------------------
+// | saithink [ saithink快速开发框架 ]
+// +----------------------------------------------------------------------
+// | Author: sai <1430792918@qq.com>
+// +----------------------------------------------------------------------
+namespace plugin\saiadmin\basic;
+
+use think\Model;
+
+/**
+ * 模型基类-非软删除
+ * @package plugin\saiadmin\basic
+ */
+class BaseNormalModel extends Model
+{
+    // 添加时间
+    protected $createTime = 'create_time';
+    // 更新时间
+    protected $updateTime = 'update_time';
+    // 隐藏字段
+    protected $hidden = ['delete_time'];
+    // 只读字段
+    protected $readonly = ['created_by', 'create_time'];
+
+    /**
+     * 时间范围搜索
+     */
+    public function searchCreateTimeAttr($query, $value)
+    {
+        $query->whereTime('create_time', 'between', $value);
+    }
+
+    /**
+     * 新增前
+     */
+    public static function onBeforeInsert($model)
+    {
+        $info = getCurrentInfo();
+        $info && $model->setAttr('created_by', $info['id']);
+    }
+
+    /**
+     * 写入前
+     */
+    public static function onBeforeWrite($model)
+    {
+        $info = getCurrentInfo();
+        $info && $model->setAttr('updated_by', $info['id']);
+    }
+
+}
