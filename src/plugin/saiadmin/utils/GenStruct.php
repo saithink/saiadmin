135e7c9c4ff16209db75fc2e43ce210ca9fcb36c
sai <1430792918@qq.com>
2024-05-16 21:57:58 +0800

    优化代码生成功能，包含app类型和plugin两种类型模板，生成文件结构优化

--- src/plugin/saiadmin/utils/GenStruct.php
+++ src/plugin/saiadmin/utils/GenStruct.php
@@ -13,42 +13,116 @@ const DS = DIRECTORY_SEPARATOR;
 class GenStruct
 {
     /**
-     * 模板saiadmin 文件生成结构
+     * 模板 文件生成结构
      * @param $table
      * @return array
      */
-    public static function saiadmin($table): array
+    public static function app($table): array
     {
         $struct = [];
         // php
+        $plugin_path = 'php'.DS.'app'.DS.$table['namespace'].DS;
+        if ($table['package_name'] != '') {
+            $php_temp = DS.$table['package_name'].DS;
+        } else {
+            $php_temp = DS;
+        }
         $struct[] = [
             'input' => 'php/controller.stub',
-            'output' => $table['namespace'].DS.'controller'.DS.$table['package_name'].DS.$table['class_name'].'Controller.php'
+            'output' => $plugin_path.DS.'controller'.$php_temp.$table['class_name'].'Controller.php'
         ];
         $struct[] = [
             'input' => 'php/logic.stub',
-            'output' => $table['namespace'].DS.'logic'.DS.$table['package_name'].DS.$table['class_name'].'Logic.php'
+            'output' => $plugin_path.DS.'logic'.$php_temp.$table['class_name'].'Logic.php'
         ];
         $struct[] = [
             'input' => 'php/model.stub',
-            'output' => $table['namespace'].DS.'model'.DS.$table['package_name'].DS.$table['class_name'].'.php'
+            'output' => $plugin_path.DS.'model'.$php_temp.$table['class_name'].'.php'
         ];
         $struct[] = [
             'input' => 'php/validate.stub',
-            'output' => $table['namespace'].DS.'validate'.DS.$table['package_name'].DS.$table['class_name'].'Validate.php'
+            'output' => $plugin_path.DS.'validate'.$php_temp.$table['class_name'].'Validate.php'
         ];
         $struct[] = [
             'input' => 'php/route.stub',
-            'output' => 'app'.DS.'config'.DS.'route.php'
+            'output' => 'php'.DS.'config'.DS.'route.generate.txt'
         ];
+
         // vue
+        if ($table['package_name'] != '') {
+            $vue_path = 'src'.DS.'views'.DS.$table['namespace'].DS.$table['package_name'].DS.$table['business_name'].DS;
+            $js_path = 'src'.DS.'views'.DS.$table['namespace'].DS.'api'.DS.$table['package_name'].DS.$table['business_name'];
+        } else {
+            $vue_path = 'src'.DS.'views'.DS.$table['namespace'].DS.$table['business_name'].DS;
+            $js_path = 'src'.DS.'views'.DS.$table['namespace'].DS.'api'.DS.$table['business_name'];
+        }
         $struct[] = [
             'input' => 'js/api.stub',
-            'output' => 'src'.DS.'api'.DS.$table['package_name'].DS.$table['business_name'].'.js'
+            'output' => $js_path.'.js'
         ];
         $struct[] = [
             'input' => 'vue'.DS.$table['tpl_category'].'.stub',
-            'output' => 'src'.DS.'views'.DS.$table['package_name'].DS.$table['business_name'].DS.'index.vue'
+            'output' => $vue_path.'index.vue'
+        ];
+        // sql
+        $struct[] = [
+            'input' => 'sql/sql.stub',
+            'output' => 'sql'.DS.'sql.sql'
+        ];
+        return $struct;
+    }
+
+    /**
+     * 模板 文件生成结构
+     * @param $table
+     * @return array
+     */
+    public static function plugin($table): array
+    {
+        $struct = [];
+        // php
+        $plugin_path = 'php'.DS.'plugin'.DS.$table['namespace'].DS;
+        if ($table['package_name'] != '') {
+            $php_temp = DS.$table['package_name'].DS;
+        } else {
+            $php_temp = DS;
+        }
+        $struct[] = [
+            'input' => 'php/controller.stub',
+            'output' => $plugin_path.'app'.DS.'controller'.$php_temp.$table['class_name'].'Controller.php'
+        ];
+        $struct[] = [
+            'input' => 'php/logic.stub',
+            'output' => $plugin_path.'app'.DS.'logic'.$php_temp.$table['class_name'].'Logic.php'
+        ];
+        $struct[] = [
+            'input' => 'php/model.stub',
+            'output' => $plugin_path.'app'.DS.'model'.$php_temp.$table['class_name'].'.php'
+        ];
+        $struct[] = [
+            'input' => 'php/validate.stub',
+            'output' => $plugin_path.'app'.DS.'validate'.$php_temp.$table['class_name'].'Validate.php'
+        ];
+        $struct[] = [
+            'input' => 'php/route.stub',
+            'output' => $plugin_path.'config'.DS.'route.generate.txt'
+        ];
+
+        // vue
+        if ($table['package_name'] != '') {
+            $vue_path = 'src'.DS.'views'.DS.$table['namespace'].DS.$table['package_name'].DS.$table['business_name'].DS;
+            $js_path = 'src'.DS.'views'.DS.$table['namespace'].DS.'api'.DS.$table['package_name'].DS.$table['business_name'];
+        } else {
+            $vue_path = 'src'.DS.'views'.DS.$table['namespace'].DS.$table['business_name'].DS;
+            $js_path = 'src'.DS.'views'.DS.$table['namespace'].DS.'api'.DS.$table['business_name'];
+        }
+        $struct[] = [
+            'input' => 'js/api.stub',
+            'output' => $js_path.'.js'
+        ];
+        $struct[] = [
+            'input' => 'vue'.DS.$table['tpl_category'].'.stub',
+            'output' => $vue_path.'index.vue'
         ];
         // sql
         $struct[] = [
