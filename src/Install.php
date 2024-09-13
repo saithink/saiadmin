b2dd771f5681cb0baec66ee1b8a845a0548a860b
sai <1430792918@qq.com>
2024-09-13 15:37:22 +0800

    自动添加more方法-3.0.5

--- src/Install.php
+++ src/Install.php
@@ -19,6 +19,7 @@ class Install
     public static function install()
     {
         static::installByRelation();
+        static::addMethod();
     }
 
     /**
@@ -66,6 +67,68 @@ class Install
                 continue;
             }
             remove_dir($path);
+        }
+    }
+
+    /**
+     * addMethod
+     * @return void
+     */
+    public static function addMethod()
+    {
+        $path = base_path() . '/support/Request.php';
+        $instance = new \support\Request("");
+
+        if (!method_exists($instance, 'more')) {
+            // 如果Request中没有more方法，则自动添加一个
+            $content = file_get_contents($path);
+            if ($content !== false) {
+                $pattern = '/class\s+(\w+)\s+extends\s+(\\\\?(?:\w+\\\\)*\w+)\s*\{[\s\S]*?\}/';
+                if (preg_match($pattern, $content, $matches, PREG_OFFSET_CAPTURE)) {
+                    $classEnd = $matches[0][1] + strlen($matches[0][0]) - 1;
+                    // 构造新方法
+                    $newMethod = <<<EOF
+    /**
+     * 获取参数增强方法
+     * @param array \$params
+     * @return array
+     */
+    public function more(array \$params): array
+    {
+        \$p = [];
+        foreach (\$params as \$param) {
+            if (!is_array(\$param)) {
+                \$p[\$param] = \$this->input(\$param);
+            } else {
+                if (!isset(\$param[1])) \$param[1] = '';
+                if (is_array(\$param[0])) {
+                    \$name = \$param[0][0] . '/' . \$param[0][1];
+                    \$keyName = \$param[0][0];
+                } else {
+                    \$name =  \$param[0];
+                    \$keyName = \$param[0];
+                }
+                \$p[\$keyName] = \$this->input(\$name, \$param[1]);
+            }
+        }
+        return \$p;
+    }
+
+
+EOF;
+                    // 在类的末尾插入新方法
+                    $newContent = substr_replace($content, $newMethod, $classEnd, 0);
+
+                    // 将修改后的内容写回文件
+                    if (file_put_contents($path, $newContent) === false) {
+                       echo "无法写入文件：$path";
+                    }
+
+                    echo "Request类添加more方法成功\n";
+                } else {
+                    echo "Request类添加方法失败，请手动添加more方法";
+                }
+            }
         }
     }
     
