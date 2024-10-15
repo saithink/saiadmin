b5c246e17374f175bd19fb404ec4e036a7473dc3
saithink <gogo2018_tech@163.com>
2024-10-15 14:25:08 +0800

    修改websocket为官方的webman/push

--- src/plugin/saiadmin/config/process.php
+++ src/plugin/saiadmin/config/process.php
@@ -2,10 +2,5 @@
 return [
     'task'  => [
         'handler'  => plugin\saiadmin\process\Task::class
-    ],
-    'websocket'  => [
-        'handler'  => plugin\saiadmin\process\WebSocket::class,
-        'listen'  => 'websocket://0.0.0.0:9527',
-        'count'   => 1,
-    ],
+    ]
 ];
--- src/plugin/saiadmin/process/Task.php
+++ src/plugin/saiadmin/process/Task.php
@@ -6,6 +6,7 @@
 // +----------------------------------------------------------------------
 namespace plugin\saiadmin\process;
 
+use Webman\Push\Api;
 use Workerman\Crontab\Crontab;
 use plugin\saiadmin\app\logic\system\SystemCrontabLogic;
 
@@ -28,6 +29,29 @@ class Task
     {
         echo '任务调用：'.date('Y-m-d H:i:s')."\n";
         var_dump('参数:'. $args);
-        return '任务调用：'.date('Y-m-d H:i:s')."\n";
+
+        $api = new Api(
+            'http://127.0.0.1:3232',
+            config('plugin.webman.push.app.app_key'),
+            config('plugin.webman.push.app.app_secret')
+        );
+        // 给订阅 saiadmin 的所有客户端推送 message 事件的消息
+        $return_ret = [
+            'event' => 'ev_new_message',
+            'message' => '新消息通知',
+            'data' => [
+                [
+                    'id' => 1,
+                    'title' => '系统消息',
+                    'content' => '欢迎使用saiadmin框架',
+                    'create_time' => date('Y-m-d H:i:s'),
+                    'send_user' => [
+                        'nickname' => '系统管理员',
+                        'avatar' => ''
+                    ]
+                ]
+            ]
+        ];
+        $api->trigger('saiadmin', 'message', $return_ret);
     }
 }
