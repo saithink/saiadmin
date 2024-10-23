95674ac291f92e85ca7f9af65c826825bed084d5
saithink <gogo2018_tech@163.com>
2024-10-23 10:06:58 +0800

    数据库菜单增加is_layout

--- src/plugin/saiadmin/db/saiadmin.sql
+++ src/plugin/saiadmin/db/saiadmin.sql
@@ -968,4 +968,8 @@ CREATE TABLE `eb_system_mail` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=1 COMMENT='邮件记录' ROW_FORMAT = Dynamic;
 
+--  eb_system_menu 添加is_layout字段
+ALTER TABLE `eb_system_menu`
+ADD COLUMN `is_layout` tinyint(1) UNSIGNED NULL DEFAULT '1' COMMENT '继承layout' AFTER `is_hidden`;
+
 SET FOREIGN_KEY_CHECKS = 1;
