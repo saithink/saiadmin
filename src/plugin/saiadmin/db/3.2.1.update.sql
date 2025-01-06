b74a02cd337d546df904d99c32b0d13a6e09f809
saithink <gogo2018_tech@163.com>
2025-01-06 23:40:56 +0800

    调整菜单图标

---
+++ src/plugin/saiadmin/db/3.2.1.update.sql
@@ -0,0 +1,22 @@
+-- 修改菜单图标
+
+UPDATE `eb_system_menu` SET `icon` = 'bi:person-lock' WHERE `eb_system_menu`.`id` = 1000;
+UPDATE `eb_system_menu` SET `icon` = 'bi:people' WHERE `eb_system_menu`.`id` = 1100;
+UPDATE `eb_system_menu` SET `icon` = 'bi:layout-sidebar' WHERE `eb_system_menu`.`id` = 1200;
+UPDATE `eb_system_menu` SET `icon` = 'bi:diagram-3-fill' WHERE `eb_system_menu`.`id` = 1300;
+UPDATE `eb_system_menu` SET `icon` = 'bi:person-heart' WHERE `eb_system_menu`.`id` = 1400;
+UPDATE `eb_system_menu` SET `icon` = 'bi:person-fill-gear' WHERE `eb_system_menu`.`id` = 1500;
+UPDATE `eb_system_menu` SET `icon` = 'bi:ui-radios' WHERE `eb_system_menu`.`id` = 2100;
+UPDATE `eb_system_menu` SET `icon` = 'bi:window-stack' WHERE `eb_system_menu`.`id` = 2200;
+UPDATE `eb_system_menu` SET `icon` = 'bi:database-fill-gear' WHERE `eb_system_menu`.`id` = 2300;
+UPDATE `eb_system_menu` SET `icon` = 'bi:megaphone' WHERE `eb_system_menu`.`id` = 2700;
+UPDATE `eb_system_menu` SET `icon` = 'bi:app-indicator' WHERE `eb_system_menu`.`id` = 3000;
+UPDATE `eb_system_menu` SET `icon` = 'bi:gear-wide-connected' WHERE `eb_system_menu`.`id` = 3200;
+UPDATE `eb_system_menu` SET `icon` = 'bi:journal-album' WHERE `eb_system_menu`.`id` = 3300;
+UPDATE `eb_system_menu` SET `icon` = 'bi:person-fill-up' WHERE `eb_system_menu`.`id` = 3400;
+UPDATE `eb_system_menu` SET `icon` = 'bi:list-ul' WHERE `eb_system_menu`.`id` = 3500;
+UPDATE `eb_system_menu` SET `icon` = 'bi:envelope-at' WHERE `eb_system_menu`.`id` = 3600;
+UPDATE `eb_system_menu` SET `icon` = 'bi:tools' WHERE `eb_system_menu`.`id` = 4000;
+UPDATE `eb_system_menu` SET `icon` = 'bi:terminal-plus' WHERE `eb_system_menu`.`id` = 4200;
+UPDATE `eb_system_menu` SET `icon` = 'bi:clock' WHERE `eb_system_menu`.`id` = 4400;
+UPDATE `eb_system_menu` SET `icon` = 'bi:gear' WHERE `eb_system_menu`.`id` = 4500;
