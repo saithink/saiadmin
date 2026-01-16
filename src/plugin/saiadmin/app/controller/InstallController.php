e7b50f8ab452fe36b095bf9d3eef1170c58d525a
saithink <gogo2018_tech@163.com>
2026-01-16 16:32:12 +0800

    安装默认使用file缓存

--- src/plugin/saiadmin/app/controller/InstallController.php
+++ src/plugin/saiadmin/app/controller/InstallController.php
@@ -250,7 +250,7 @@ EOF;
 <?php
 return [
     // 默认缓存驱动
-    'default' => env('CACHE_MODE', 'redis'),
+    'default' => env('CACHE_MODE', 'file'),
     // 缓存连接方式配置
     'stores'  => [
         // redis缓存
